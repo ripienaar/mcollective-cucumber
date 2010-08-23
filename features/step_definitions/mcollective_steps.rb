ee24c652941f5d56ebc55ca86facffb38a565a7b
R.I.Pienaar <rip@devco.net>
2010-08-23 22:22:51 +0100

    Add checks for the other error codes

--- features/step_definitions/mcollective_steps.rb
+++ features/step_definitions/mcollective_steps.rb
@@ -35,6 +35,26 @@ Then /the result should not be an error/ do
     raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 0
 end
 
+Then /the request should have been aborted/ do
+    raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 1
+end
+
+Then /the request should fail due to unknown action/ do
+    raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 2
+end
+
+Then /the request should fail due to missing data/ do
+    raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 3
+end
+
+Then /the request should fail due to invalid data/ do
+    raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 4
+end
+
+Then /the request should fail due to an unknown error/ do
+    raise "Reply statuscode == #{@reply[:statuscode]}" unless @reply[:statuscode] == 5
+end
+
 Then /^the following parameters should be received as a result:$/ do |table|
     data = @reply[:data]
 
